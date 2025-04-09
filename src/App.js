2cc8ca2544c7cc903c22539ec635cf978b6f80e4
Vishnu <vish.2005.in@gmail.com>
2025-04-09 21:36:00 +0530

    5 commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import RegisterPage from './pages/RegisterPage';
 import './App.css';
 import UserPage from './pages/UserPage.js';
 import AdminPage from './pages/AdminPage';
-
+import TeamViewPage from './pages/TeamViewPage.js';
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         <Route path="/AdminPage" element={<AdminPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        
+        <Route path="/TeamViewPage" element={<TeamViewPage />} />
       </Routes>
     </div>
   );
